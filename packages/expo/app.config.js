712bac4ff1ae36be04bd4b99ec26b9cc4cee897d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-09-24 20:29:22 +0900

    chore: set version

--- packages/expo/app.config.js
+++ packages/expo/app.config.js
@@ -1,6 +1,7 @@
 /** @type {import("@expo/config-types/build/ExpoConfig").ExpoConfig} */
 const EXPO_CONFIG = {
   name: "expo-vite-webview",
+  version: "0.0.0",
   extra: {
     // TODO: switch by environment variable
     DEV_WEB_VIEW_PORT: "18182",
