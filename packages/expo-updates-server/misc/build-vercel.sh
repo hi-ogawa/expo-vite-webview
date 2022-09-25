8357f6cfbf7824df820749978411f29ef91691cd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-09-26 01:15:44 +0900

    chore: add build script

--- packages/expo-updates-server/misc/build-vercel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eu -o pipefail
-
-CONFIG_RAKKAS_ADAPTER=vercel npm run build:vercel
-# cp -f misc/vercel/config.json .vercel/output/config.json
-# cp -f misc/vercel/.vc-config.json .vercel/output/functions/index.func/.vc-config.json
